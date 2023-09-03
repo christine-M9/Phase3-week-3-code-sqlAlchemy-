586785301590729783a17ae426d400cb9508a938
Christine <christinekhekama99@gmail.com>
2023-09-03 16:30:30 +0300

    added ORMs

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+engine = create_engine('sqlite:///my_database.db')
+Session = sessionmaker(bind=engine)
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -11,9 +13,14 @@ class Restaurant(Base):
     name = Column(String, nullable=False)
     price = Column(Integer, nullable=False)
 
-    # relationship to reviews
     reviews = relationship('Review', back_populates='restaurant')
     customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+
+    def reviews(self):
+        return [review for review in self.reviews]
+
+    def customers(self):
+        return [customer for customer in self.customers]
 
 class Customer(Base):
     __tablename__ = 'customers'
@@ -22,9 +29,14 @@ class Customer(Base):
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
 
-    # relationship to reviews
     reviews = relationship('Review', back_populates='customer')
     restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+
+    def reviews(self):
+        return [review for review in self.reviews]
+
+    def restaurants(self):
+        return [restaurant for restaurant in self.restaurants]
 
 class Review(Base):
     __tablename__ = 'reviews'
@@ -34,6 +46,15 @@ class Review(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
     customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
 
-    # relationship to restaurant and customer
     restaurant = relationship('Restaurant', back_populates='reviews')
     customer = relationship('Customer', back_populates='reviews')
+
+    def customer(self):
+        return self.customer
+
+    def restaurant(self):
+        return self.restaurant
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    Session().close()
---
+++ seed.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Restaurant, Customer, Review
+from faker import Faker 
+
+# Creating an SQLite database engine
+engine = create_engine('sqlite:///my_database.db')
+
+# Creatng a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Initialize Faker
+fake = Faker()
+
+# Creating some fake restaurants
+for _ in range(5):
+    restaurant = Restaurant(name=fake.company(), price=fake.random_int(min=1, max=5))
+    session.add(restaurant)
+
+# Creating some fake customers
+for _ in range(10):
+    customer = Customer(first_name=fake.first_name(), last_name=fake.last_name())
+    session.add(customer)
+
+# Creating some fake reviews
+for _ in range(20):
+    review = Review(star_rating=fake.random_int(min=1, max=5),
+                    restaurant=fake.random_element(elements=session.query(Restaurant).all()),
+                    customer=fake.random_element(elements=session.query(Customer).all()))
+    session.add(review)
+
+# Commiting the changes to the database
+session.commit()
+
+# Closing the session
+session.close()
