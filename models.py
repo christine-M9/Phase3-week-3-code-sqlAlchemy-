2beb0875c6b91105a35d867083dbb24dce005208
Christine <christinekhekama99@gmail.com>
2023-09-03 12:10:10 +0300

    creating SQL models and migration

---
+++ models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+
+    # relationship to reviews
+    reviews = relationship('Review', back_populates='restaurant')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+
+    # relationship to reviews
+    reviews = relationship('Review', back_populates='customer')
+    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer, nullable=False)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
+    customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
+
+    # relationship to restaurant and customer
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
